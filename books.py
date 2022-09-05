684cd116da417f887287df8f6d652d0d92570799
JIHOJU <jihoju96@gmail.com>
2022-09-05 11:36:24 +0900

    project1: Post Request

--- books.py
+++ books.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from typing import Optional
-from enum import Enum
 
 app = FastAPI()
 
@@ -13,15 +12,6 @@ BOOKS = {
 }
 
 
-class DirectionName(str, Enum):
-    north = "North"
-    south = "South"
-    east = "East"
-    west = "West"
-
-
-# async def read_all_books(skip_book: str = "book_3"):
-# async def read_all_books(skip_book: str):
 @app.get('/')
 async def read_all_books(skip_book: Optional[str] = None):
     if skip_book:
@@ -36,29 +26,15 @@ async def read_book(book_name: str):
     return BOOKS[book_name]
 
 
-@app.get("/directions/{direction_name}")
-async def get_direction(direction_name: DirectionName):
-    if direction_name == DirectionName.north:
-        return {"Direction": direction_name, "sub": "Up"}
-    if direction_name == DirectionName.south:
-        return {"Direction": direction_name, "sub": "Down"}
-    if direction_name == DirectionName.west:
-        return {"Direction": direction_name, "sub": "Left"}
-    if direction_name == DirectionName.east:
-        return {"Direction": direction_name, "sub": "Right"}
+@app.post("/")
+async def create_book(book_title, book_author):
+    current_book_id = 0
 
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if x > current_book_id:
+                current_book_id = x
 
-@app.get("/books/mybook")
-async def read_favorite_book():
-    return {"book_title": "My favorite book"}
-
-
-@app.get("/books/{book_id}")
-async def read_book(book_id: int):
-    """
-        book_id: int => FastAPI 에서 자동으로 string->integer 로 변환 but, int 형태가 아닌
-        다른 'foo' 같은 변수가 들어가면 에러 발생
-    :param book_id: integer type variable of book_id
-    :return:
-    """
-    return {"book_title": book_id}
+    BOOKS[f'book_{current_book_id + 1}'] = {'title': book_title, 'author': book_author}
+    return BOOKS[f'book_{current_book_id + 1}']
