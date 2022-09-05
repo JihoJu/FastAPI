03a6d882c2665c0132d4a09037b0b8af23047ca3
JIHOJU <jihoju96@gmail.com>
2022-09-05 11:10:44 +0900

    project1: path parameters

--- books.py
+++ books.py
@@ -14,3 +14,19 @@ BOOKS = {
 @app.get('/')
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/mybook")
+async def read_favorite_book():
+    return {"book_title": "My favorite book"}
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    """
+        book_id: int => FastAPI 에서 자동으로 string->integer 로 변환 but, int 형태가 아닌
+        다른 'foo' 같은 변수가 들어가면 에러 발생
+    :param book_id: integer type variable of book_id
+    :return:
+    """
+    return {"book_title": book_id}
