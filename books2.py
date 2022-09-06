f4d2850537f5b42923b2061ef70699edc1398147
JIHOJU <jihoju96@gmail.com>
2022-09-06 19:37:16 +0900

    project2: Form Fields

--- books2.py
+++ books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, HTTPException, Request, status, Form
 from typing import Optional
 from pydantic import BaseModel, Field
 from uuid import UUID
@@ -56,6 +56,11 @@ async def negative_number_exception_handler(request: Request,
         content={"message": f"Hey, why do you want {exc.books_to_return} "
                             f"books? You need to read more!"},
     )
+
+
+@app.post("/books/login")
+async def book_login(username: str = Form(), password: str = Form()):
+    return {"username": username, "password": password}
 
 
 @app.get("/")
