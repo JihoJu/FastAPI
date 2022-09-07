07c958ce273e8292fe5476876fc1affe84ef5ea4
JIHOJU <jihoju96@gmail.com>
2022-09-07 19:21:39 +0900

    project3: Get All Todos from Database

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-from database import engine
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
 import models as models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -8,6 +9,21 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+"""
+- Session: Manages persistence operations for ORM-mapped objects.
+- Dependency Injection이란 코드의 재활용을 위해 제공해주는 fastapi의 기능
+    만약 두 개의 path operation이 있다면 모두 같은 기능을 한다
+    -> 그럼 2 개를 모두 쓰는 것보단 하나의 기능으로 정의 후 재활용을 하면 효율적일 것이다.
+    -> Depends 사용!! 
+"""
 @app.get("/")
-async def create_database():
-    return {"Database": "Created"}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
