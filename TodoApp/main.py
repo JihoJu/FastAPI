8deebbd74a951e402512174fd888ad82f0ab76de
JIHOJU <jihoju96@gmail.com>
2022-09-07 19:33:35 +0900

    project3: Get Todo by ID

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from database import engine, SessionLocal
-import models as models
+import models
 from sqlalchemy.orm import Session
 
 app = FastAPI()
@@ -24,6 +24,21 @@ def get_db():
     -> 그럼 2 개를 모두 쓰는 것보단 하나의 기능으로 정의 후 재활용을 하면 효율적일 것이다.
     -> Depends 사용!! 
 """
+
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+    raise http_exception()
+
+
+def http_exception():
+    return HTTPException(status_code=404, detail="Todo not found")
